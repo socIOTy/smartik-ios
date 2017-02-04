e3ef21264f85c86d195759035583d7c4fe0666ed
serhiipianykh <serhiipianykh@gmail.com>
2017-02-04 15:47:41 -0500

    Initial Commit

---
+++ smARTIK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smARTIK
+//
+//  Created by Serhii Pianykh on 2017-02-04.
+//  Copyright © 2017 socIOTy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
