76130e999e19bc59496f37c75085bf533b8a12ca
serhiipianykh <serhiipianykh@gmail.com>
2017-02-15 20:28:08 -0500

    Light intensity and RGB implemented

--- smARTIK/ViewController.swift
+++ smARTIK/ViewController.swift
@@ -12,15 +12,39 @@ import ArtikCloudSwift3
 class ViewController: UIViewController {
     
     @IBOutlet weak var statulLbl: UILabel!
+    
+    
+    @IBOutlet weak var intensitySlider: UISlider!
+    @IBOutlet weak var rColorSlider: UISlider!
+    @IBOutlet weak var gColorSlider: UISlider!
+    @IBOutlet weak var bColorSlider: UISlider!
+    
+    
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var rColorLabel: UILabel!
+    @IBOutlet weak var gColorLabel: UILabel!
+    @IBOutlet weak var bColorLabel: UILabel!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let intensityLevel = Int(round(intensitySlider.value))
+        let rColor = Int(round(rColorSlider.value))
+        let gColor = Int(round(gColorSlider.value))
+        let bColor = Int(round(bColorSlider.value))
+        
+        intensityLabel.text = "Intensity: \(intensityLevel)"
+        
+        rColorLabel.text = "R: \(rColor)"
+        gColorLabel.text = "G: \(gColor)"
+        bColorLabel.text = "B: \(bColor)"
+        
+        colorView.backgroundColor = UIColor(red: CGFloat(rColor/255), green: CGFloat(gColor/255), blue: CGFloat(bColor/255), alpha: CGFloat(intensityLevel/100))
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func loginPressed(_ sender: Any) {
         Authentification.artikCloudLogin()
@@ -49,5 +73,85 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    
+    @IBAction func intensityChanging(_ sender: UISlider) {
+        intensityLabel.text = "Intensity: \(Int(round(intensitySlider.value)))"
+        colorView.backgroundColor = UIColor(red: CGFloat(rColorSlider.value/255), green: CGFloat(gColorSlider.value/255), blue: CGFloat(bColorSlider.value/255), alpha: CGFloat(intensitySlider.value/100))
+    }
+    
+    
+    @IBAction func rColorChanging(_ sender: UISlider) {
+        rColorLabel.text = "R: \(Int(round(rColorSlider.value)))"
+        colorView.backgroundColor = UIColor(red: CGFloat(rColorSlider.value/255), green: CGFloat(gColorSlider.value/255), blue: CGFloat(bColorSlider.value/255), alpha: CGFloat(intensitySlider.value/100))
+    }
+    
+    
+    @IBAction func gColorChanging(_ sender: UISlider) {
+        gColorLabel.text = "G: \(Int(round(gColorSlider.value)))"
+        colorView.backgroundColor = UIColor(red: CGFloat(rColorSlider.value/255), green: CGFloat(gColorSlider.value/255), blue: CGFloat(bColorSlider.value/255), alpha: CGFloat(intensitySlider.value/100))
+    }
+    
+    @IBAction func bColorChanging(_ sender: UISlider) {
+        bColorLabel.text = "B: \(Int(round(bColorSlider.value)))"
+        colorView.backgroundColor = UIColor(red: CGFloat(rColorSlider.value/255), green: CGFloat(gColorSlider.value/255), blue: CGFloat(bColorSlider.value/255), alpha: CGFloat(intensitySlider.value/100))
+    }
+    
+    
+    @IBAction func colorChangingEnds(_ sender: UISlider) {
+        let actions: Actions = Actions()
+        let actionArray : ActionArray = ActionArray()
+        let action = Action()
+        
+        action.name = "setColorAsRGB"
+        
+        let bulbColorRGB: [String:Int] = [
+            "r":Int(round(rColorSlider.value)),
+            "g":Int(round(gColorSlider.value)),
+            "b":Int(round(bColorSlider.value))
+        ]
+        
+        action.parameters = [
+            "colorRGB": bulbColorRGB,
+            "intensity": Int(round(intensitySlider.value))
+        ]
+        
+        actionArray.actions = []
+        actionArray.actions!.append(action)
+        actions.data = actionArray
+        actions.ddid = Metadata.deviceID
+        
+        MessagesAPI.sendActions(data: actions).then {
+            response -> Void in
+            //self.statulLbl.text = response.data?.mid!
+            }.catch{error -> Void in
+                print(String(format: "%s", String(describing: error)))
+        }
+
+    }
+    
+    @IBAction func intensityChangingEnds(_ sender: UISlider) {
+        let actions: Actions = Actions()
+        let actionArray : ActionArray = ActionArray()
+        let action = Action()
+        
+        action.name = "setIntensity"
+    
+        action.parameters = ["intensity" : Int(round(intensitySlider.value))]
+        
+        actionArray.actions = []
+        actionArray.actions!.append(action)
+        actions.data = actionArray
+        actions.ddid = Metadata.deviceID
+        
+        MessagesAPI.sendActions(data: actions).then {
+            response -> Void in
+            //self.statulLbl.text = response.data?.mid!
+            }.catch{error -> Void in
+                print(String(format: "%s", String(describing: error)))
+        }
+    }
+    
+    
 }
 
